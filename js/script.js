558768996b2e7f632f26d862c0242f5d159b5e90
pontus-wernqvist-nti-johanneberg <92986737+pontus-wernqvist-nti-johanneberg@users.noreply.github.com>
2022-04-03 22:46:59 +0200

    Lagt till film

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const burger = document.querySelector('.burger') //variabel som är min document query selector för burger
-const nav = document.querySelector('.site_navigation_links') // skapar konstant variabel med namn nav som tilldelas det första elementet i HTML-dokumentet som har selektorn '.navlinks'
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.site_navigation_links') 
 
 function toggleMenu() {
-    nav.classList.toggle('upper_nav-active') //   
+    nav.classList.toggle('upper_nav-active') 
     burger.classList.toggle('toggle')
 }
 
-burger.addEventListener('click', toggleMenu)
+burger.addEventListener('click', toggleMenu)    
